f2ff3abf266117c7ca76b874282f2b36019e5d90
ciobiano <rafaelobiano@yahoo.com>
2023-10-23 19:01:18 +0100

    bug fixes 🪲⚒️

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,6 +1,5 @@
 import prismadb from "@/lib/prismadb";
 import { stripe } from "@/lib/stripe";
-import { formatter } from "@/lib/utils";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
